5ef44307f74fe340ff3663b425ca16f77bee8c66
TaracoPasta <s.yanase0226@gmail.com>
2025-02-21 11:44:33 +0900

    [add] 週単位の検索

--- src/search_arxiv/arxiv_search_client.py
+++ src/search_arxiv/arxiv_search_client.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
 import arxiv
 from dataclasses import dataclass
 
 from src.params.load_from_yml import load_from_yml
 
+
+###############################################################
+# Dataclasses for Arxiv search query and parameters
+###############################################################
 @dataclass
 class ArxivSearchQuery:
     """Dataclass for Arxiv search query
@@ -35,10 +40,10 @@ class ArxivSearchParameters:
     sort_by: str
     sort_order: str
     id_list: list
-    
+
 
 class ArxivSearchClient:
-    def __init__(self, search_params: ArxivSearchParameters, multi_threading_enabled: bool = False, multi_threading_chunk_size: int = 10, multi_threading_num_workers: int = 16):
+    def __init__(self, search_query: ArxivSearchQuery, search_params: ArxivSearchParameters, multi_threading_enabled: bool = False, multi_threading_chunk_size: int = 10, multi_threading_num_workers: int = 16):
         """Initialize Arxiv search client
 
         Args:
@@ -52,6 +57,7 @@ class ArxivSearchClient:
         self.multi_threading_num_workers = multi_threading_num_workers
         self.search_results = []
         
+        self.search_query = search_query
         self.search_params = search_params
         
     @classmethod
@@ -67,22 +73,98 @@ class ArxivSearchClient:
         # load search parameters from YAML
         dict_search_params = load_from_yml(path)
         # prepare search query
-        search_query = str(ArxivSearchQuery(**dict_search_params['query']))
+        search_query = ArxivSearchQuery(**dict_search_params['query'])
+        search_query_str = str(search_query)
         # prepare search options
         search_options = dict_search_params['options']
-        search_params = ArxivSearchParameters(query=search_query, **search_options)
+        search_params = ArxivSearchParameters(query=search_query_str, **search_options)
         
         # prepare multi-threading options
         multi_threading_enabled = dict_search_params.get('enabled', False)
         multi_threading_chunk_size = dict_search_params.get('chunk_size', 1000)
         multi_threading_num_workers = dict_search_params.get('num_workers', 10)
-        return cls(search_params, multi_threading_enabled, multi_threading_chunk_size, multi_threading_num_workers)
+        return cls(search_query, search_params, multi_threading_enabled, multi_threading_chunk_size, multi_threading_num_workers)
         
+    #####################
+    # Setters
+    #####################
+    def set_search_params_options(self, key: str, value: str):
+        """Set search parameters options
+
+        Args:
+            key (str): key
+            value (str): value
+        """        
+        setattr(self.search_params, key, value)
+        
+    def set_search_query_options(self, key: str, value: str):
+        """Set search query options
+
+        Args:
+            key (str): key
+            value (str): value
+        """        
+        setattr(self.search_query, key, value)
+        # update search params
+        self.set_search_params_options('query', str(self.search_query))
+        
+    #####################
+    # Getters
+    #####################
+    def get_search_params_options(self, key: str):
+        """Get search parameters options
+
+        Args:
+            key (str): key
+
+        Returns:
+            str: value
+        """        
+        return getattr(self.search_params, key)
+    
+    def get_search_query_options(self, key: str):
+        """Get search query options
+
+        Args:
+            key (str): key
+
+        Returns:
+            str: value
+        """        
+        return getattr(self.search_query, key)
+    
+    
+    #####################
+    # Search
+    #####################
     def apply_query(self):
         """Apply query to Arxiv search API
 
         Returns:
             arxiv.arxiv.ResultList: Arxiv search results
         """        
-        return arxiv.query(**self.search_params.__dict__)
+        self.search_results = arxiv.query(**self.search_params.__dict__)
+        
+        return self.search_results
+        
+    def search_weekly(self):
+        """Search Arxiv for weekly updates
+
+        Returns:
+            arxiv.arxiv.ResultList: Arxiv search results
+        """        
+        # get current date
+        current_date = datetime.now().date()
+        # get last week date
+        last_week_date = current_date - timedelta(days=7)
+        
+        # convert date to string YYYYMMDDHHMMSS
+        last_week_date = last_week_date.strftime("%Y%m%d%H%M%S")
+        current_date = current_date.strftime("%Y%m%d%H%M%S")
+        
+        # update search query with date range
+        self.set_search_query_options('submittedDate_from', last_week_date)
+        self.set_search_query_options('submittedDate_to', current_date)
+        
+        return self.apply_query()
         
